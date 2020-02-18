c27e2c1d1e535dbc5b68920aad9a71e9ce7d7f38
Bram <bramet2@live.nl>
2020-02-18 10:53:18 +0100

    W.I.P Push

--- app/app.py
+++ app/app.py
@@ -1,13 +1,14 @@
 import os
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'HELLO WORLD!'
+def index():
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
