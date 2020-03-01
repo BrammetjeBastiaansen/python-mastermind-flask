2cf766969119ccf9f29c8e12c7c1ce0424d44a90
BC46 <cs.melis@student.avans.nl>
2020-03-01 15:06:04 +0100

    add database

--- app/app.py
+++ app/app.py
@@ -1,13 +1,20 @@
 import os
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET KEY'] = 'Exfe29yxyYWkc71p1cB/vQ=='
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///site.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+from models import Player, Game
 
 
 @app.route('/')
 def index():
-    return "Test Page :ED"
+    return "<h1>Test Page :ED</h1>"
 
 
 if __name__ == '__main__':
