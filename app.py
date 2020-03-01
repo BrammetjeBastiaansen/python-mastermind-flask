c00be2d7e2d036ffaadc20df9df7a008e8ed8065
BC46 <cs.melis@student.avans.nl>
2020-03-01 19:37:09 +0100

    add database

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import os
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///site.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+from models import Player, Game
 
 
 @app.route('/')
