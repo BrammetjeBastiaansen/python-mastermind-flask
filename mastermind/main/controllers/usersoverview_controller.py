61ebbe54a13b6bb4f98566826a177f3ae361107d
BC46 <cs.melis@student.avans.nl>
2020-03-10 12:27:50 +0100

    fix useroverview and usersoverview pages

---
+++ mastermind/main/controllers/usersoverview_controller.py
@@ -0,0 +1,11 @@
+from flask import request, redirect, render_template
+from flask.views import MethodView
+from mastermind.main.models.player_model import Player_Model
+
+
+class Usersoverview_Controller(MethodView):
+    def __init__(self):
+        self._player_model = Player_Model()
+
+    def get(self):
+        return render_template("usersoverview.html")
--- mastermind/main/main_routes.py
+++ mastermind/main/main_routes.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, render_template
 from .controllers.player_controller import Player_Controller
+from .controllers.usersoverview_controller import Usersoverview_Controller
+from .controllers.useroverview_controller import Useroverview_Controller
+
 
 main_bp = Blueprint('main_bp', __name__,
                     template_folder='templates',
@@ -10,4 +13,5 @@ main_bp = Blueprint('main_bp', __name__,
 main_bp.add_url_rule("/", view_func=Player_Controller.as_view("index"), methods=["GET"])
 main_bp.add_url_rule("/", view_func=Player_Controller.as_view("create"), methods=["POST"])
 
-main_bp.add_url_rule("/usersoverview", view_func=Player_Controller.as_view("usersoverview"), methods=["GET"])
+main_bp.add_url_rule("/usersoverview", view_func=Usersoverview_Controller.as_view("usersoverview"), methods=["GET"])
+main_bp.add_url_rule("/useroverview", view_func=Useroverview_Controller.as_view("useroverview"), methods=["GET"])
