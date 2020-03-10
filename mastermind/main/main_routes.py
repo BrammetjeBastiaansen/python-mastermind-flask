af850f3ec6d347a4f8ab1d792d4bf3262881454a
Bram <bramet2@live.nl>
2020-03-10 11:31:14 +0100

    Added startpage + imrpoved entire project structure

--- mastermind/main/main_routes.py
+++ mastermind/main/main_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-from .endpoints.PlayerAPI import PlayerAPI
+from .controllers.player_controller import Player_Controller
 
 main_bp = Blueprint('main_bp', __name__,
                     template_folder='templates',
@@ -7,5 +7,5 @@ main_bp = Blueprint('main_bp', __name__,
 
 
 # Routes
-main_bp.add_url_rule("/", view_func=PlayerAPI.as_view("index"), methods=["GET"])
-main_bp.add_url_rule("/create", view_func=PlayerAPI.as_view("create"), methods=["POST"])
+main_bp.add_url_rule("/", view_func=Player_Controller.as_view("index"), methods=["GET"])
+main_bp.add_url_rule("/", view_func=Player_Controller.as_view("create"), methods=["POST"])
