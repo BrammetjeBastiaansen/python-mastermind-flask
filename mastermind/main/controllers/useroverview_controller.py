43b685c6ad3d8436154d2267df6bc4c2f4dd2250
BC46 <cs.melis@student.avans.nl>
2020-03-16 00:50:16 +0100

    make user and user overview pages dynamic

--- mastermind/main/controllers/useroverview_controller.py
+++ mastermind/main/controllers/useroverview_controller.py
@@ -7,5 +7,5 @@ class Useroverview_Controller(MethodView):
     def __init__(self):
         self._player_model = Player_Model()
 
-    def get(self):
-        return render_template("useroverview.html")
+    def get(self, id):
+        return render_template("useroverview.html", player=self._player_model.get_player_by_id(id), game_amounts_by_date=self._player_model.get_played_game_amounts_by_date(id))
