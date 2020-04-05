43c830c8cf162b3801c3b82538c25cf5f3624112
Bram <bramet2@live.nl>
2020-04-06 01:17:38 +0200

    Disabled thread check

--- mastermind/database/database.py
+++ mastermind/database/database.py
@@ -2,12 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///database/site.db', convert_unicode=True)
+engine = create_engine('sqlite:///database/site.db', convert_unicode=True, connect_args={"check_same_thread": False})
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
+
 
 def init_db():
     # import all modules here that might define models so that
