24ee87eeb9d1dcbf9704d79d4ca658e64e154d14
C <cs.melis@student.avans.nl>
2020-04-06 03:13:43 +0200

    Add expire on commit param to session maker

--- mastermind/database/database.py
+++ mastermind/database/database.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 engine = create_engine('sqlite:///database/site.db', convert_unicode=True, connect_args={"check_same_thread": False})
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
+                                         expire_on_commit=False,
                                          bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
