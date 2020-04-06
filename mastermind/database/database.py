a90064b6d4b73c7308061094e800d96eb6b1d07f
Bram <bramet2@live.nl>
2020-04-06 03:14:27 +0200

    Removed comments

--- mastermind/database/database.py
+++ mastermind/database/database.py
@@ -12,8 +12,5 @@ Base.query = db_session.query_property()
 
 
 def init_db():
-    # import all modules here that might define models so that
-    # they will be registered properly on the metadata.  Otherwise
-    # you will have to import them first before calling init_db()
     import mastermind.database.models
     Base.metadata.create_all(bind=engine)
